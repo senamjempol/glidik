7d8ad4d011c9e8bb9bf07c34182bb48d4b23093f
senamjempol <48201831+senamjempol@users.noreply.github.com>
2021-12-21 13:14:04 +0700

    Update start-screen.sh

--- start-screen.sh
+++ start-screen.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 
-screen -S 1.38 -d -m ./lolMiner --algo BEAM-III --pool stratum+tcp://beamv3.usa-east.nicehash.com:3387 --user 3H1YkJhw9Ybgii4H1D35usRPCeRn7G81SK.buruh --cclk 400
+screen -S 1.38 -d -m ./lolMiner --algo BEAM-III --pool stratum+tcp://beamv3.usa-east.nicehash.com:3387 --user 3H1YkJhw9Ybgii4H1D35usRPCeRn7G81SK.glidik --cclk 900
