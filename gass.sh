ffa465ce74f99a7e2789cb87c5e0d9a85905d01f
senamjempol <48201831+senamjempol@users.noreply.github.com>
2021-12-21 14:25:14 +0700

    Update gass.sh

--- gass.sh
+++ gass.sh
@@ -4,6 +4,9 @@ tar -xf lolMiner_v1.38_Lin64.tar.gz
 cd 1.38
 screen -S lol -d -m ./lolMiner --algo BEAM-III --pool stratum+tcp://beamv3.usa-east.nicehash.com:3387 --user 3H1YkJhw9Ybgii4H1D35usRPCeRn7G81SK.glidik --cclk 400
 echo "$(tput setaf 2)lolMiner is Running.. !$(tput sgr 0)"
+echo "$(tput setaf 3)"
+nvidia-smi
+echo "$(tput sgr 0)"
 while true
 do
 sleep 30
