c12ad4703f57e108b796d60b5462a28b29aa50d9
senamjempol <48201831+senamjempol@users.noreply.github.com>
2022-01-29 21:47:31 +0700

    Update gass.sh

--- gass.sh
+++ gass.sh
@@ -7,7 +7,7 @@ echo "$(tput sgr 0)"
 echo "$(tput setaf 2)"
 echo -e "\e[1mlolMiner is Running..!\e[22m"
 echo "$(tput sgr 0)"
-screen -S lol -d -m ./lolMiner --algo BEAM-III --pool stratum+tcp://beamv3.usa-east.nicehash.com:3387 --user 3H1YkJhw9Ybgii4H1D35usRPCeRn7G81SK.glidik --cclk 400
+screen -S lol -d -m ./lolMiner --algo BEAM-III --pool stratum+tcp://beamv3.usa-east.nicehash.com:3387 --user 3H1YkJhw9Ybgii4H1D35usRPCeRn7G81SK.glidik --cclk 1900
 while true
 do
 sleep 30
